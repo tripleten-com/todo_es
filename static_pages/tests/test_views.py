4d78b455bf3a62cd27827c262c1206c2321fb803
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-01-31 12:11:39 +0300

    Update test_views.py
    added english

--- static_pages/tests/test_views.py
+++ static_pages/tests/test_views.py
@@ -7,12 +7,12 @@ class StaticViewsTests(TestCase):
         self.guest_client = Client()
 
     def test_about_page_accessible_by_name(self):
-        """URL, генерируемый при помощи имени static_pages:about, доступен."""
+        """URL generated using the name static_pages:about, available."""
         response = self.guest_client.get(reverse('static_pages:about'))
         self.assertEqual(response.status_code, 200)
 
     def test_about_page_uses_correct_template(self):
-        """При запросе к staticpages:about
-        применяется шаблон staticpages/about.html."""
+        """When sending a staticpages:about
+        the template is applied staticpages/about.html."""
         response = self.guest_client.get(reverse('static_pages:about'))
         self.assertTemplateUsed(response, 'static_pages/about.html')
