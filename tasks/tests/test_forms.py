ad4910a6f6f236d257c77d53a5fe077b64ecc342
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-01-31 12:19:51 +0300

    Update test_forms.py
    added english

--- tasks/tests/test_forms.py
+++ tasks/tests/test_forms.py
@@ -8,45 +8,45 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
-# Создаем временную папку для медиа-файлов;
-# на момент теста медиа папка будет переопределена
+# Create a temporary folder for media files
+# We'll override the media folder for the test
 TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 
-# Для сохранения media-файлов в тестах будет использоватьсяgs
-# временная папка TEMP_MEDIA_ROOT, а потом мы ее удалим
+# We'll use the temporary folder TEMP_MEDIA_ROOT
+# for saving media files during the test, and then we'll delete it
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class TaskCreateFormTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Создаем запись в базе данных для проверки сушествующего slug
+        # Create a database record to validate the existing slug
         Task.objects.create(
             title='Тестовый заголовок',
             text='Тестовый текст',
             slug='first'
         )
-        # Создаем форму, если нужна проверка атрибутов
+        # Create a form if you need to validate the attributes
         cls.form = TaskCreateForm()
 
     @classmethod
     def tearDownClass(cls):
         super().tearDownClass()
-        # Модуль shutil - библиотека Python с прекрасными инструментами 
-        # для управления файлами и директориями: 
-        # создание, удаление, копирование, перемещение, изменение папок и файлов
-        # Метод shutil.rmtree удаляет директорию и всё её содержимое
+        # shutil is a Python library with useful tools
+        # for managing files and directories, including 
+        # creating, deleting, copying, moving, and editing
+        # The shutil.rmtree method deletes the directory and all its content
         shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
 
     def setUp(self):
-        # Создаем неавторизованный клиент
+        # Create an unauthorized client
         self.guest_client = Client()
 
     def test_create_task(self):
-        """Валидная форма создает запись в Task."""
-        # Подсчитаем количество записей в Task
+        """Valid form creates a record in Task."""
+        # Count the number of records in Task
         tasks_count = Task.objects.count()
-        # Подготавливаем данные для передачи в форму
+        # To test image upload,
         small_gif = (
             b'\x47\x49\x46\x38\x39\x61\x01\x00'
             b'\x01\x00\x00\x00\x00\x21\xf9\x04'
@@ -60,8 +60,8 @@ class TaskCreateFormTests(TestCase):
             content_type='image/gif'
         )
         form_data = {
-            'title': 'Тестовый заголовок',
-            'text': 'Тестовый текст',
+            'title': 'Test title',
+            'text': 'Test body',
             'image': uploaded,
         }
         response = self.guest_client.post(
@@ -69,54 +69,54 @@ class TaskCreateFormTests(TestCase):
             data=form_data,
             follow=True
         )
-        # Проверяем, сработал ли редирект
+        # Test if the redirect works
         self.assertRedirects(response, reverse('tasks:task_added'))
-        # Проверяем, увеличилось ли число постов
+        # Check if the number of posts increased
         self.assertEqual(Task.objects.count(), tasks_count+1)
-        # Проверяем, что создалась запись с нашим слагом
+        # Check that the record with the specified slug has been created
         self.assertTrue(
             Task.objects.filter(
                 slug='testovyij-zagolovok',
-                text='Тестовый текст',
+                text='Test body',
                 image='tasks/small.gif'
                 ).exists()
         )
 
     def test_cant_create_existing_slug(self):
-        # Подсчитаем количество записей в Task
+        # Count the number of records in Task
         tasks_count = Task.objects.count()
         form_data = {
-            'title': 'Заголовок из формы',
-            'text': 'Текст из формы',
-            'slug': 'first',  # отправим в форму slug, который уже есть в БД
+            'title': 'Title from the form',
+            'text': 'Body from the form',
+            'slug': 'first',  # Try to submit a slug already existing in the database
         }
-        # Отправляем POST-запрос
+        # Send a POST request
         response = self.guest_client.post(
             reverse('tasks:home'),
             data=form_data,
             follow=True
         )
-        # Убедимся, что запись в базе данных не создалась:
-        # сравним количество записей в Task до и после отправки формы
+        # Make sure that the database record was not created
+        # Compare the number of records in Task before and after submitting the form
         self.assertEqual(Task.objects.count(), tasks_count)
-        # Проверим, что форма вернула ошибку с ожидаемым текстом:
-        # из объекта responce берём словарь 'form',
-        # указываем ожидаемую ошибку для поля 'slug' этого словаря
+        # Check that the form returned an appropriate error:
+        # take the 'form' dictionary from the response object
+        # and specify the expected error for the 'slug' field of this dictionary
         self.assertFormError(
             response,
             'form',
             'slug',
-            'Адрес "first" уже существует, придумайте уникальное значение'
+            'Slug "first" already exists, enter a unique slug'
         )
-        # Проверим, что ничего не упало и страница отдаёт код 200
+        # Check that the site didn't crash and that the page returns a 200 status code
         self.assertEqual(response.status_code, 200)
 
-    # Если не тестировали содержимое лейблов в models
-    # или переопределили их при создании формы - тестируем так:
+    # If you haven't tested the contents of labels in models
+    # or redefined them when creating the form - we test like this:
     def test_title_label(self):
         title_label = TaskCreateFormTests.form.fields['title'].label
-        self.assertEqual(title_label, 'Заголовок')
+        self.assertEqual(title_label, 'Title')
 
     def test_title_help_text(self):
         title_help_text = TaskCreateFormTests.form.fields['title'].help_text
-        self.assertEqual(title_help_text, 'Дайте короткое название задаче')
+        self.assertEqual(title_help_text, 'Enter task name')
