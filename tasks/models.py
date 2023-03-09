c49ebb27f70ecb99cab9b65f06ac9d2ab8e968a5
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-03-09 17:02:34 +0300

    Update models.py
    added spanish

--- tasks/models.py
+++ tasks/models.py
@@ -1,40 +1,40 @@
 from django.db import models
-# You need to install the pytils library: pip install pytils.
+# Necesitas instalar la librería pytils: pip install pytils.
 from pytils.translit import slugify
 
 
 class Task(models.Model):
     title = models.CharField(
-        'Title',
-        default='Default value',
+        'Título',
+        default='Valores predeterminados',
         max_length=100,
-        help_text='Enter task name'
+        help_text='Introduce el nombre de la tarea'
     )
     text = models.TextField(
-        'Текст',
-        help_text='Enter task description'
+        'Texto',
+        help_text='Introduce la descripción de la tarea'
     )
     slug = models.SlugField(
-        'URL slug for the task page',
+        'El slug de la URL para la página de la tarea',
         max_length=100,
         unique=True,
         blank=True,
-        help_text=('Enter a unique URL for the task page. Use only '
-                   'Latin characters, numbers, hyphens, and underscores')
+        help_text=('Introduce una URL única para la página de la tarea. Utiliza solo '
+                   'Caracteres latinos, números, guiones y guiones bajos')
     )
     image = models.ImageField(
-        'Image',
+        'Imagen',
         upload_to='tasks/',
         blank=True,
         null=True,
-        help_text='Upload an image'
+        help_text='Sube una imagen'
     )
 
     def __str__(self):
         return self.title
 
-    # Extend the default save() method: if the slug field is not specified,
-    # transliterate the title field value as Latin characters
+    # Amplía el método save() por defecto: si no se especifica el campo slug,
+    # transliterar el valor del campo del título en caracteres latinos (100 caracteres como máximo)
     # (100 characters max)
     def save(self, *args, **kwargs):
         if not self.slug:
