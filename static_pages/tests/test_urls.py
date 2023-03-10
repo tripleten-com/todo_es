4ec577051562e0ded7f97cf0164c8c4c5000488c
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-03-10 09:55:32 +0300

    Update test_urls.py
    added spanish

--- static_pages/tests/test_urls.py
+++ static_pages/tests/test_urls.py
@@ -6,11 +6,11 @@ class StaticURLTests(TestCase):
         self.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
-        """Test the availability of /page/about/."""
+        """Prueba la disponibilidad de /page/about/."""
         response = self.guest_client.get('/page/about/')
         self.assertEqual(response.status_code, 200)
 
     def test_about_url_uses_correct_template(self):
-        """Test the template for /page/about/."""
+        """Prueba la plantilla para /page/about/."""
         response = self.guest_client.get('/page/about/')
         self.assertTemplateUsed(response, 'static_pages/about.html')
