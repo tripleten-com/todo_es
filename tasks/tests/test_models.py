48fb5bb283c46932bee187811aefceb22dde34b0
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-03-10 09:48:34 +0300

    Update test_models.py
    added spanish

--- tasks/tests/test_models.py
+++ tasks/tests/test_models.py
@@ -8,27 +8,27 @@ class TaskModelTest(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Create a test record in the database and
-        # save it as a class variable
-        # We do not specify the slug value, we expect that when creating
-        # it will be created automatically from the title.
-        # And we will make the title so that after transliteration it becomes
-        # more than 100 characters
+        # Crear un registro en la base de datos de prueba
+        # guárdalo como variable de clase
+        # No especificamos el valor de slug, esperamos que al crear
+        # se creará automáticamente a partir del título.
+        # Y haremos el título para que después de la transliteración se convierta en
+        # más de 100 caracteres
         #
         cls.task = Task.objects.create(
             title='I am a str'*10,
-            text='Test body'
+            text='Cuerpo de prueba'
         )
         # print(len(cls.task.title))
 
     def test_verbose_name(self):
-        """verbose_name of the fields matches the expected values."""
+        """verbose_name de los diferentes campos coinciden con los valores previstos."""
         task = TaskModelTest.task
         field_verboses = {
-            'title': 'Title',
-            'text': 'Text',
-            'slug': 'URL slug for the task page',
-            'image': 'Image',
+            'title': 'Título',
+            'text': 'Texto',
+            'slug': 'El slug de la URL para la página de la tarea',
+            'image': 'Imagen',
         }
         for value, expected in field_verboses.items():
             with self.subTest(value=value):
@@ -36,14 +36,14 @@ class TaskModelTest(TestCase):
                     task._meta.get_field(value).verbose_name, expected)
 
     def test_help_text(self):
-        """help_text of the fields matches the expected values."""
+        """help_text en los diferentes campos coinciden con los valores previstos."""
         task = TaskModelTest.task
         field_help_texts = {
-            'title': 'Enter task name',
-            'text': 'Enter task description',
-            'slug': ('Enter a unique URL for the task page. Use only '
-                     'Latin characters, numbers, hyphens, and underscores'),
-            'image': 'Upload an image',
+            'título': 'Introduce el nombre de la tarea',
+            'texto': 'Introduce la descripción de la tarea',
+            'slug': ('Introduce una URL única para la página de la tarea. Utiliza solo '
+                     'Caracteres latinos, números, guiones y guiones bajos'),
+            'imagen': 'Sube una imagen',
         }
         for value, expected in field_help_texts.items():
             with self.subTest(value=value):
@@ -51,14 +51,14 @@ class TaskModelTest(TestCase):
                     task._meta.get_field(value).help_text, expected)
 
     def test_text_convert_to_slug(self):
-        """The value of title is converted into slug."""
+        """El valor de title se convierte en slug."""
         task = TaskModelTest.task
         slug = task.slug
         self.assertEquals(slug, 'i-am-a-str'*10)
 
     def test_text_slug_max_length_not_exceed(self):
         """
-        A longer slug is truncated to avoid exceeding the slug model field max_length.
+        Un slug más largo se trunca para evitar que se supere el campo max_length del modelo de slug.
         """
         task = TaskModelTest.task
         max_length_slug = task._meta.get_field('slug').max_length
@@ -66,7 +66,7 @@ class TaskModelTest(TestCase):
         self.assertEqual(max_length_slug, length_slug)
 
     def test_object_name_is_title_fild(self):
-        """The __str__ field of the task object contains the value of the task.title field."""
+        """El campo __str__ del objeto task contiene el valor de la tarea.campo de título."""
         task = TaskModelTest.task
         expected_object_name = task.title
         self.assertEqual(expected_object_name, str(task))
