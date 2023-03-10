d3abaa42c336486f92721d5993c3d5eb97c22c68
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-03-10 09:42:02 +0300

    Update test_urls.py
    added spanish

--- tasks/tests/test_urls.py
+++ tasks/tests/test_urls.py
@@ -11,63 +11,63 @@ class TaskURLTests(TestCase):
     def setUpClass(cls):
         super().setUpClass()
 
-        # Create a database record to check the availability of task/test-slug/
+        # Crea un registro en la base de datos para comprobar la disponibilidad de task/test-slug/
         Task.objects.create(
-            title='Test title',
-            text='Test body',
+            title='Título de prueba',
+            text='Cuerpo de prueba',
             slug='test-slug',
         )
 
     def setUp(self):
-        # Create an unauthorized client
+        # Crea un cliente no autorizado
         self.guest_client = Client()
-        # Create an authorized client
-        self.user = User.objects.create_user(username='StasBasov')
+        # Crea un cliente autorizado
+        self.user = User.objects.create_user(username='NoraStrong')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
 
-    # Test the pages available to all users
+    # Probar las páginas disponibles para todos los usuarios
     def test_home_url_exists_at_desired_location(self):
-        """Page / is available to all users."""
+        """La página / está disponible para todos los usuarios."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_task_added_url_exists_at_desired_location(self):
-        """Page /added/ is available to all users."""
+        """La página /added/ está disponible para todos los usuarios."""
         response = self.guest_client.get('/added/')
         self.assertEqual(response.status_code, 200)
 
-    # Check the availability of pages to authorized users
+    # Comprueba la disponibilidad de las páginas para los usuarios autorizados
     def test_task_list_url_exists_at_desired_location(self):
-        """Page /task/ is available to authorized users."""
+        """La página /task/ está disponible para los usuarios autorizados."""
         response = self.authorized_client.get('/task/')
         self.assertEqual(response.status_code, 200)
 
     def test_task_detail_url_exists_at_desired_location_authorized(self):
-        """Page /task/test-slug/ is available to authorized users."""
+        """La página /task/test-slug/ está disponible para los usuarios autorizados."""
         response = self.authorized_client.get('/task/test-slug/')
         self.assertEqual(response.status_code, 200)
 
-    # Check the redirects for unauthorized users
+    # Comprueba si hay usuarios no autorizados en las redirecciones
     def test_task_list_url_redirect_anonymous_on_admin_login(self):
-        """Page /task/ redirects unauthorized
-        users to the login page.
+        """La página /task/ redirige a usuarios no 
+        autorizados a la página de inicio.
         """
         response = self.guest_client.get('/task/', follow=True)
         self.assertRedirects(
             response, '/admin/login/?next=/task/')
 
     def test_task_detail_url_redirect_anonymous_on_admin_login(self):
-        """Page /task/test_slug/ redirects unauthorized
-        users to the login page.
+        """La página /task/test_slug/ redirecciona a usuarios no 
+        autorizados a la página de inicio.
         """
         response = self.client.get('/task/test-slug/', follow=True)
         self.assertRedirects(
             response, ('/admin/login/?next=/task/test-slug/'))
 
-    # Check the templates for each path
+    # Comprueba las plantillas de cada ruta
     def test_urls_uses_correct_template(self):
-        """URL uses the corresponding template."""
+        """URL utiliza la plantilla correspondiente."""
         templates_url_names = {
             'tasks/home.html': '/',
             'tasks/added.html': '/added/',
